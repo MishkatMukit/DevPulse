a850401e71e5e64d4cf479aad5a1ee4d9eb05213
MishkatMukit <mahabubmishkat8@gmail.com>
2026-05-22 22:22:11 +0600

    Initialize database connection pool with PostgreSQL

---
+++ src/DB/index.db.ts
@@ -0,0 +1,42 @@
+import { Pool } from "pg"
+import config from "../config"
+
+export const pool = new Pool({
+    connectionString: config.connction_string
+})
+
+export const initDB = async () => {
+    try {
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS users (
+        id SERIAL PRIMARY KEY,
+        name VARCHAR(32),
+        email VARCHAR(32) UNIQUE NOT NULL,
+        password TEXT NOT NULL,
+        role VARCHAR(20) DEFAULT 'contributor' CHECK(role IN ('contributor', 'maintainer')),
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )
+        `)
+
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS issues (
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(150) NOT NULL,
+        description TEXT CHECK(LENGTH(description)>=20),
+        type VARCHAR(20) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+        status VARCHAR(15) NOT NULL DEFAULT 'open'
+        CHECK (status IN ('open', 'in_progress', 'resolved')),
+        reporter_id INT REFERENCES users(id) ON DELETE CASCADE,
+
+        created_at TIMESTAMP DEFAULT NOW(),
+        updated_at TIMESTAMP DEFAULT NOW()
+        )   
+            `)
+
+        console.log("database connected");
+    } catch (error) {
+        console.log(error);
+    }
+}
